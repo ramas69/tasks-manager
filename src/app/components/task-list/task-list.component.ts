dfb6ee1377e2c9e18d5e87107e4ee329b127f55c
rama69 <ramatoulaye.sou@gmail.com>
2025-01-27 20:56:39 +0100

    feat: création du composant task-list pour l'affichage des tâches

---
+++ src/app/components/task-list/task-list.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { Task } from '../../models/task.interface';
+import { TaskService } from '../../services/task.service';
+import { OnInit } from '@angular/core';
+import { NgFor } from '@angular/common';
+
+@Component({
+  selector: 'app-task-list',
+  standalone: true,
+  imports: [NgFor],
+  templateUrl: './task-list.component.html',
+  styleUrl: './task-list.component.css'
+})
+export class TaskListComponent implements OnInit {
+
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.tasks = this.taskService.getTasks();
+  }
+
+
+}
